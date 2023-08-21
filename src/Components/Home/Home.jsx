2631cdf93e32ee7d9bb7b77b52e9ffbff6d58bff
Emoncr <biplobemon 75945@gmail.com>
2023-08-22 01:29:39 +0600

    FIX- fixed bg image error issue

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../Header/Header";
-import "./Home.css";
+
 import Hero from "../Hero/Hero";
 const Home = () => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,26 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('./src/images/hero_bg.png')",
+        "hero-pattern":
+          "url('https://storage.googleapis.com/msgsndr/vJVPkXKI0ujMBULtlF3Q/media/64e3b8e7d6dc5bbef57458ed.png')",
       },
       fontFamily: {
-      'display': ['KoHo',],
+        display: ["KoHo"],
       },
       colors: {
-        'white': '#ffffff',
-        'yellow': '#F9A51A',
-        'black':'#000',
-        Amber:{
-          '600' : '#F9A51A'
-        }
+        white: "#ffffff",
+        yellow: "#F9A51A",
+        black: "#000",
+        Amber: {
+          600: "#F9A51A",
+        },
       },
-
     },
   },
   plugins: [],
-})
-
+});
